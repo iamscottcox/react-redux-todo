44cdb341cf7bdd785ea4327e3632a556a020941e
Scott Cox <scottcox89@gmail.com>
2016-12-29 17:05:28 +0000

    yarn add: redux-promise, redux-logger. Changed configureStore.js to fit

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -8,7 +8,6 @@ import TodoList from '../components/TodoList'
 class VisibleTodoList extends Component {
   componentDidMount() {
     this.fetchData();
-    console.log(this.props.onTodoClick)
   }
   componentDidUpdate(prevProps) {
     if (this.props.filter !== prevProps.filter) {
