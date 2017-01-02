de7a60077f321bde9b03cd4ed2b511b47dc28f6c
Scott Cox <scottcox89@gmail.com>
2017-01-02 21:41:08 +0000

    Comment and spacing changes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -23,9 +23,9 @@ export const getVisibleTodos = (state, filter) => {
 };
 
 export const getIsFetching = (state, filter) => {
-  return fromList.getIsFetching(state.listByFilter[filter]);
+  return fromList.getIsFetching(state.listByFilter[ filter ]);
 };
 
 export const getErrorMessage = (state, filter) => {
-  return fromList.getErrorMessage(state.listByFilter[filter]);
+  return fromList.getErrorMessage(state.listByFilter[ filter ]);
 };
