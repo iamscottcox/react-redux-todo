1aa5b8e444a03be91e97ed0182169e0e3f27928b
Scott Cox <scottcox89@gmail.com>
2017-01-03 09:23:09 +0000

    Added comments to createList.js

--- src/reducers/createList.js
+++ src/reducers/createList.js
@@ -3,24 +3,33 @@ import { combineReducers } from 'redux'
 const createList = (filter) => {
 
   const handleToggle = (state, action) => {
+    // Get action.response.result as toggledId
+    // Get action.response.entities as entities
     const { result: toggledId, entities } = action.response;
+    // Get entities.todos[toggledId] as completed
     const { completed } = entities.todos[toggledId];
+    // Return Boolean for active or completed todos
     const shouldRemove = (
       (completed && filter === 'active') ||
       (!completed && filter === 'completed')
     );
     return shouldRemove ?
+      // If true, return all todos apart from the matched todos
       state.filter(id => id !== toggledId) :
+      // Otherwise, just return the state
       state;
   };
 
   const ids = (state = [], action) => {
     switch (action.type) {
       case 'FETCH_TODOS_SUCCESS':
+        // Filter is passed in a the root of the function
         return filter === action.filter ?
+          // Return an array of results where filter matches action.filter
           action.response.result :
           state;
       case 'ADD_TODO_SUCCESS':
+        console.log(action);
         return filter !== 'completed' ?
           [...state, action.response.result] :
           state;
