a10df68174f4fd43dd501b996e27667acda57f7e
Scott Cox <scottcox89@gmail.com>
2017-01-07 11:27:19 +0100

    Added comments to reducers/byId.js

--- src/reducers/byId.js
+++ src/reducers/byId.js
@@ -1,13 +1,17 @@
 const byId = (state = {}, action) => {
+  // If successful...
   if (action.response) {
+    // ...return the state with the todos spread onto the end
     return {
       ...state,
       ...action.response.entities.todos,
     };
   }
+  // Else return the state
   return state;
 };
 
 export default byId;
 
+// Selector for getting todos from their state by ID
 export const getTodo = (state, id) => state[id];
