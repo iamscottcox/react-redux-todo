1e19036c8bfa9ff57a00db94ca912e100053642c
Scott Cox <scottcox89@gmail.com>
2017-01-07 11:32:06 +0100

    Added comments to components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
+// params comes from React-Router
 const App = ({ params }) => (
   <div id="TodoApp">
     <Header />
