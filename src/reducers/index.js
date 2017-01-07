ca46f6ecddd18789ab4ed6af312b644db03e7597
Scott Cox <scottcox89@gmail.com>
2017-01-07 11:21:39 +0100

    Added comments to reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import byId, * as fromById from './byId';
 import createList, * as fromList from './createList';
 
-
+// Reducer listByFilter uses createList to make more reducers for the state object
 const listByFilter = combineReducers({
   all: createList('all'),
   active: createList('active'),
@@ -17,16 +17,20 @@ const todos = combineReducers({
 export default todos;
 
 export const getVisibleTodos = (state, filter) => {
+  // Returns an array of objects by getting all IDs using getIds()...
   const ids = fromList.getIds(state.listByFilter[ filter ]);
+  // ...then using those IDs with getTodos().
   return ids.map(id => {
     return fromById.getTodo(state.byId, id);
   });
 };
 
 export const getIsFetching = (state, filter) => {
+  // Uses getIsFetching() to get the current filter's fetching state
   return fromList.getIsFetching(state.listByFilter[ filter ]);
 };
 
 export const getErrorMessage = (state, filter) => {
+  // Uses getErrorMessage() to get the current filter's error message
   return fromList.getErrorMessage(state.listByFilter[ filter ]);
 };
