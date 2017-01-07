bbf242bb0173f10f308b2b8368350b2a7c471dad
Scott Cox <scottcox89@gmail.com>
2017-01-07 11:30:04 +0100

    Added comments to configureStore.js

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,15 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
-import todoApp from './reducers/index';
+import todos from './reducers/index';
 
 const configureStore = () => {
+  // Defines the middlewares to be used
   const middlewares = [ thunk ];
+  // If production, push logger into middlewares
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(createLogger());
   }
+  // Return a created store using todos from reducers/index and the defined middlewares
   return createStore(
-    todoApp,
+    todos,
     applyMiddleware(...middlewares)
   );
 };
